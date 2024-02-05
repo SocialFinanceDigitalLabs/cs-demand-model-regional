8ace2046ac3e34c3aa1b0b4ba805903591e2a2af
AmyNickollse <amy.nickolls@googlemail.com>
2024-02-05 17:16:50 +0000

    chart displays predict output

--- dm_regional_app/views.py
+++ dm_regional_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 import plotly.express as px
 from dm_regional_app.predictor import predict
+import pandas as pd
 
 
 def chart_view(request):
@@ -13,10 +14,14 @@ def chart_view(request):
     print(df)
 
     # TODO - use df to create a plotly figure
-    x = [1, 2, 3]
-    y = [4, 1, 2]
-    fig = px.bar(x=x, y=y)
-    fig.update_layout(title="Other Plot")
+    
+    
+    df_pp = df.unstack().reset_index()
+    df_pp.columns = ["from", "date", "value"]
+    print(df_pp)
+
+    fig = px.line(df_pp, y="value", x="date", color="from")
+    fig.update_layout(title="Prediction")
     fig_html = fig.to_html(full_html=False)
 
     return render(request, "dm_regional_app/chart.html", context={"chart": fig_html})
