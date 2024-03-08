6e734c9cfd0a4d0de49fb3e2a396c02dda08c850
Francisco Macedo <franciscovcbm@gmail.com>
2024-03-08 11:56:13 +0000

    add bootstrap datepicker

--- dm_regional_app/forms.py
+++ dm_regional_app/forms.py
@@ -1,14 +1,15 @@
+from bootstrap_datepicker_plus.widgets import DatePickerInput
 from django import forms
-    
+
 
 class PredictFilter(forms.Form):
     start_date = forms.DateField(
-        widget=forms.widgets.DateInput(attrs={'type': 'date'}),
-        label='Start Date',
+        widget=DatePickerInput(),
+        label="Start Date",
         required=True,
     )
     end_date = forms.DateField(
-        widget=forms.widgets.DateInput(attrs={'type': 'date'}),
-        label='End Date',
+        widget=DatePickerInput(range_from="start_date"),
+        label="End Date",
         required=True,
     )
