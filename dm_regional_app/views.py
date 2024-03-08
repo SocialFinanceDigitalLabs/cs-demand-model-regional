cedd1266c25ec7d16922bf78597a0bed878880b3
Francisco Macedo <franciscovcbm@gmail.com>
2024-03-08 11:58:35 +0000

    add start of historic view

--- dm_regional_app/views.py
+++ dm_regional_app/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render
 
 from dm_regional_app.charts import prediction_chart
-from dm_regional_app.forms import PredictFilter
+from dm_regional_app.forms import HistoricDataFilter, PredictFilter
 from dm_regional_app.models import Scenario
 from ssda903.predictor import predict
 from ssda903.reader import read_data
@@ -64,6 +64,58 @@ def prediction(request):
     chart = prediction_chart(prediction)
     return render(
         request, "dm_regional_app/views/prediction.html", {"form": form, "chart": chart}
+    )
+
+
+@login_required
+def historic_data(request):
+    if request.method == "POST":
+        # read data
+        datacontainer = read_data(source=settings.DATA_SOURCE)
+
+        # initialize form with data
+        form = HistoricDataFilter(
+            request.POST,
+            la_choices=datacontainer.unique_las,
+            placement_type_choices=datacontainer.unique_placement_types,
+        )
+        if form.is_valid():
+            data = form.apply_filters(datacontainer.enriched_view)
+        else:
+            data = datacontainer.enriched_view
+    else:
+        # read data
+        datacontainer = read_data(source=settings.DATA_SOURCE)
+
+        # initialize form with default dates
+        form = HistoricDataFilter(
+            initial={
+                "start_date": datacontainer.start_date,
+                "end_date": datacontainer.end_date,
+            },
+            la_choices=datacontainer.unique_las,
+            placement_type_choices=datacontainer.unique_placement_types,
+        )
+        data = datacontainer.enriched_view
+
+    # TODO AMY - create the relevant charts from this dataset, according to the designs
+    entry_into_care_count = data.loc[
+        data.placement_type_before
+        == datacontainer.config.PlacementCategories.NOT_IN_CARE
+    ]["CHILD"].nunique()
+    exiting_care_count = data.loc[
+        data.placement_type_after
+        == datacontainer.config.PlacementCategories.NOT_IN_CARE
+    ]["CHILD"].nunique()
+
+    return render(
+        request,
+        "dm_regional_app/views/historic.html",
+        {
+            "form": form,
+            "entry_into_care_count": entry_into_care_count,
+            "exiting_care_count": exiting_care_count,
+        },
     )
 
 
