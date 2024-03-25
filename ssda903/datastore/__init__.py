7e4e516eb7ae88827b54f029393c197b7a5d67c1
Francisco Macedo <franciscovcbm@gmail.com>
2024-03-25 12:20:38 +0000

    remove unused datastores and cli commands

--- ssda903/datastore/__init__.py
+++ ssda903/datastore/__init__.py
@@ -1,12 +1,5 @@
-import fs
-
 from ._api import DataFile, DataStore, Metadata, TableType
-from ._opener import fs_datastore
-from ._sample import SampleFSOpener
 from ._storage import StorageDataStore
-
-fs.opener.registry.install(SampleFSOpener)
-
 
 __all__ = [
     "StorageDataStore",
