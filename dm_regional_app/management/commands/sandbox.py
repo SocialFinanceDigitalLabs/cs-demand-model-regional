c314d4cca7bcf9e71e9c05753af9df2a1bdef8cd
Francisco Macedo <franciscovcbm@gmail.com>
2024-03-25 12:21:01 +0000

    fix sandbox

--- dm_regional_app/management/commands/sandbox.py
+++ dm_regional_app/management/commands/sandbox.py
@@ -5,23 +5,25 @@ import pandas as pd
 from dateutil.relativedelta import relativedelta
 from demand_model import MultinomialPredictor
 from demand_model.multinomial.predictor import Prediction
+from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.core.files.storage import default_storage
 from django.core.management.base import BaseCommand
 
-from ssda903 import Config, DemandModellingDataContainer, PopulationStats, fs_datastore
+from ssda903 import (
+    Config,
+    DemandModellingDataContainer,
+    PopulationStats,
+    StorageDataStore,
+)
 
 User = get_user_model()
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        thing = User.objects.all()
-        for t in thing:
-            print(t.profile.la)
-
-        """datastore = fs_datastore("sample://v1.zip")
+        datastore = StorageDataStore(default_storage, settings.DATA_SOURCE)
         config = Config()
         dc = DemandModellingDataContainer(datastore, config)
 
-        
-        print(dc.enriched_view)"""
+        print(dc.enriched_view)
