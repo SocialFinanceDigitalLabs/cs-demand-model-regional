8836f9966bfbe541d5f4d0aed357a2750baa3c61
AmyNickollse <amy.nickolls@googlemail.com>
2024-05-01 15:32:48 +0100

    split reference end date and prediction start input and updated references

--- ssda903/predictor.py
+++ ssda903/predictor.py
@@ -11,29 +11,36 @@ from ssda903 import Config, PopulationStats
 
 def predict(
     data: pd.DataFrame,
-    start: date,
-    end: date,
-    prediction_date: Optional[date] = None,
+    reference_start_date: date,
+    reference_end_date: date,
+    prediction_start_date: Optional[date] = None,
+    prediction_end_date: Optional[date] = None,
 ) -> Prediction:
     """
     Analyses source between start and end, and then predicts the population at prediction_date.
     """
     config = Config()
     stats = PopulationStats(data, config)
-    if prediction_date is None:
-        prediction_date = end + relativedelta(months=12)
+    if prediction_start_date is None:
+        prediction_start_date = reference_end_date
+    if prediction_end_date is None:
+        prediction_end_date = prediction_start_date + relativedelta(months=24)
     print(
-        f"Running analysis between {start} and {end} "
-        f"and predicting to {prediction_date}"
+        f"Running analysis between {reference_start_date:} and {reference_end_date} "
+        f"and predicting from {prediction_start_date} to {prediction_end_date}"
     )
 
     predictor = MultinomialPredictor(
-        population=stats.stock_at(end),
-        transition_rates=stats.raw_transition_rates(start, end),
-        transition_numbers=stats.daily_entrants(start, end),
-        start_date=end,
+        population=stats.stock_at(prediction_start_date),
+        transition_rates=stats.raw_transition_rates(
+            reference_start_date, reference_end_date
+        ),
+        transition_numbers=stats.daily_entrants(
+            reference_start_date, reference_end_date
+        ),
+        start_date=prediction_start_date,
     )
-    prediction_days = (prediction_date - end).days
+    prediction_days = (prediction_end_date - prediction_start_date).days
     prediction = predictor.predict(prediction_days, progress=False)
 
     return prediction
