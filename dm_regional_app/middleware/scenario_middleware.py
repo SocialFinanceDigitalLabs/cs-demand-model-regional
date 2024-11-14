2f7e0159be05139bf07999b88765e20e9c70e709
christina.moore <christina.moore@socialfinance.org.uk>
2024-11-14 12:31:29 +0000

    feat: redirect to home screen when no data uploads are available

--- dm_regional_app/middleware/scenario_middleware.py
+++ dm_regional_app/middleware/scenario_middleware.py
@@ -1,7 +1,17 @@
+import re
+
+from django.contrib import messages
+from django.shortcuts import redirect
+
 from dm_regional_app.models import DataSource, SessionScenario
 
 
 class SessionScenarioMiddleware:
+    PATHS_TO_IGNORE = re.compile(
+        r"(/admin.*)|(/accounts.*)|(/upload_data.*)|(/$)",
+        re.IGNORECASE,
+    )
+
     def __init__(self, get_response):
         self.get_response = get_response
 
@@ -9,7 +19,11 @@ class SessionScenarioMiddleware:
         # Initialise a SessionScenario
         session_scenario_id = request.session.get("session_scenario_id", None)
         current_user = request.user
-        if current_user.is_authenticated and DataSource.objects.exists():
+
+        if not current_user.is_authenticated:
+            return self.get_response(request)
+
+        if DataSource.objects.exists():
             historic_filters = {
                 "la": [],
                 "ethnicity": [],
@@ -49,7 +63,15 @@ class SessionScenarioMiddleware:
             )
 
             request.session["session_scenario_id"] = session_scenario.pk
+        else:
+            if self.PATHS_TO_IGNORE.match(request.path_info):
+                return self.get_response(request)
 
-        response = self.get_response(request)
+            messages.error(
+                request,
+                "There is no data available. If you are an admin, please upload data via Data Source Upload. Otherwise, "
+                "please contact your local admin.",
+            )
+            return redirect("home")
 
-        return response
+        return self.get_response(request)
