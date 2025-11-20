e401df450713e48ec74ca2f6b6731356a5794ba5
christina.moore <christina.moore@socialfinance.org.uk>
2025-11-20 17:24:05 +0000

    build: add datadog configuration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,7 +8,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 6.0"
+    }
+    datadog = {
+      source = "DataDog/datadog"
     }
   }
 
@@ -17,6 +20,12 @@ terraform {
 
 provider "aws" {
   region  = "eu-west-2"
+}
+
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+  api_url = "https://api.${var.datadog_site}"
 }
 
 resource "aws_s3_bucket" "bucket" {
@@ -147,4 +156,131 @@ resource "aws_iam_user_policy_attachment" "attachment" {
 
 resource "aws_iam_access_key" "bucket_user" {
   user    = aws_iam_user.bucket_user.name
+}
+
+### Datadog
+data "aws_iam_policy_document" "datadog_aws_integration_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::464622532012:root"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "sts:ExternalId"
+      values = [
+        "${datadog_integration_aws_account.datadog_integration.auth_config.aws_auth_config_role.external_id}"
+      ]
+    }
+  }
+}
+
+data "datadog_integration_aws_iam_permissions" "datadog_permissions" {}
+
+locals {
+  all_permissions = data.datadog_integration_aws_iam_permissions.datadog_permissions.iam_permissions
+
+  max_policy_size = 6144
+  target_chunk_size = 5900
+
+  permission_sizes = [
+    for perm in local.all_permissions :
+    length(perm) + 3
+  ]
+  cumulative_sizes = [
+    for i in range(length(local.permission_sizes)) :
+    sum(slice(local.permission_sizes, 0, i + 1))
+  ]
+
+  chunk_assignments = [
+    for cumulative_size in local.cumulative_sizes :
+    floor(cumulative_size / local.target_chunk_size)
+  ]
+  chunk_numbers = distinct(local.chunk_assignments)
+  permission_chunks = [
+    for chunk_num in local.chunk_numbers : [
+      for i, perm in local.all_permissions :
+      perm if local.chunk_assignments[i] == chunk_num
+    ]
+  ]
+}
+
+data "aws_iam_policy_document" "datadog_aws_integration" {
+  count = length(local.permission_chunks)
+
+  statement {
+    actions   = local.permission_chunks[count.index]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "datadog_aws_integration" {
+  count = length(local.permission_chunks)
+
+  name   = "DatadogAWSIntegrationPolicy-${count.index + 1}"
+  policy = data.aws_iam_policy_document.datadog_aws_integration[count.index].json
+}
+
+resource "aws_iam_role" "datadog_aws_integration" {
+  name               = "DatadogIntegrationRole"
+  description        = "Role for Datadog AWS Integration"
+  assume_role_policy = data.aws_iam_policy_document.datadog_aws_integration_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "datadog_aws_integration" {
+  count = length(local.permission_chunks)
+
+  role       = aws_iam_role.datadog_aws_integration.name
+  policy_arn = aws_iam_policy.datadog_aws_integration[count.index].arn
+}
+
+resource "aws_iam_role_policy_attachment" "datadog_aws_integration_security_audit" {
+  role       = aws_iam_role.datadog_aws_integration.name
+  policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
+}
+
+resource "datadog_integration_aws_account" "datadog_integration" {
+  account_tags   = [
+    "env:${var.app}-${var.environment}",
+    "aws_account:${var.account_id}"
+  ]
+  aws_account_id = var.account_id
+  aws_partition  = "aws"
+  aws_regions {
+    include_all = true
+  }
+  auth_config {
+    aws_auth_config_role {
+      role_name = "DatadogIntegrationRole"
+    }
+  }
+  resources_config {
+    cloud_security_posture_management_collection = true
+    extended_collection                          = true
+  }
+  traces_config {
+    xray_services {
+    }
+  }
+  logs_config {
+    # This block should be empty on initial apply and then populated after the datadog_forwarder is applied
+    # and the arn is known. This is to avoid a circular dependency. Destroy should copy the same pattern in reverse.
+    lambda_forwarder {
+      lambdas = [module.datadog_forwarder.datadog_forwarder_arn]
+      sources = ["lambda", "s3"]
+    }
+  }
+  metrics_config {
+    namespace_filters {
+    }
+  }
+}
+
+module "datadog_forwarder" {
+  source  = "DataDog/log-lambda-forwarder-datadog/aws"
+  version = "~> 1.0"
+
+  dd_api_key = var.datadog_api_key
+  dd_site    = var.datadog_site
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -5,3 +5,20 @@ variable "environment" {
 variable "app" {
   description = "The name of the application"
 }
+
+variable "account_id" {
+  description = "The AWS account ID"
+}
+
+variable "datadog_api_key" {
+  description = "Datadog API key for monitoring"
+}
+
+variable "datadog_app_key" {
+  description = "Datadog Application key for monitoring"
+}
+
+variable "datadog_site" {
+  description = "Datadog site to send metrics to"
+  default     = "datadoghq.eu"
+}
